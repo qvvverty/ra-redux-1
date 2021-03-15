fd4b0f16cedbd7a0ad3b57e036c3944bd9bec3a3
AppVeyor <ci@appveyor.com>
2021-03-15 22:54:44 +0000

    fa5b32b

---
+++ precache-manifest.e435a357fb1a3d47b5714ea227193bfd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a46574cce0926d9bbfa180fc178e5ab",
+    "url": "/ra-redux-1-2/index.html"
+  },
+  {
+    "revision": "3c9c1a7fbba0cfa1ba5d",
+    "url": "/ra-redux-1-2/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a33dcab988f970a8a0af",
+    "url": "/ra-redux-1-2/static/js/2.fa0e92c5.chunk.js"
+  },
+  {
+    "revision": "3c9c1a7fbba0cfa1ba5d",
+    "url": "/ra-redux-1-2/static/js/main.308816cb.chunk.js"
+  },
+  {
+    "revision": "3d0952182acdb48798f2",
+    "url": "/ra-redux-1-2/static/js/runtime-main.7a3cdfea.js"
+  }
+]);
